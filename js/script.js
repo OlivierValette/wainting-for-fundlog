b55bdd6bf08f9cb5e43e8356fa0339368de54f9b
Olivier Valette <olivier.valette@spi10.com>
2019-04-04 11:25:23 +0200

    Not so long to wait

---
+++ js/script.js
@@ -0,0 +1,24 @@
+// Set the date we're counting down to
+const countDownDate = new Date("May 6, 2019 18:00:00").getTime();
+
+// Update the count down every 1 second
+let countdownfunction = setInterval(function() {
+    // Delay between now an the count down date
+    let delay = countDownDate - new Date().getTime();
+    delay = delay / 1000.;
+    // Time calculations for days, hours, minutes and seconds
+    let days = Math.floor(delay / (60 * 60 * 24));
+    let hours = Math.floor((delay % (60 * 60 * 24)) / (60 * 60));
+    let minutes = Math.floor((delay % (60 * 60)) / 60);
+    let seconds = Math.floor(delay % 60);
+    // Output the result in an element with id="demo"
+    document.querySelector("#days .number").innerHTML = days.toString();
+    document.querySelector("#hours .number").innerHTML = hours.toString();
+    document.querySelector("#minutes .number").innerHTML = minutes.toString();
+    document.querySelector("#seconds .number").innerHTML = seconds.toString();
+    // If the count down is over, write some text
+    if (delay < 0) {
+        clearInterval(countdownfunction);
+        document.getElementById("countdown").innerHTML = "En retard !";
+    }
+}, 1000);
